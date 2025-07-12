6d72c93e651da96dd98200d446ddd0ddc6fad5e3
honeypathkar <honeypatkar70@gmail.com>
2025-07-12 10:36:23 +0530

    Theme setup and shadcn setup complete

---
+++ stack-it/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: "class", // this works with next-themes
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
+    },
+  },
+  plugins: [],
+};
